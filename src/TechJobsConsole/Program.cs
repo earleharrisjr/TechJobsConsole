631a82d551c65bc48f6e45ee8204961d26f4501a
Earl Harris <earleharrisjr@gmail.com>
2022-04-29 15:00:38 -0400

    finished print all jobs

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -119,6 +119,11 @@ namespace TechJobsConsole
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
             Console.WriteLine("PrintJobs is not implemented yet");
+            foreach (Dictionary<string,string> job in someJobs)
+            {
+                Console.WriteLine($"Job Name: {job["name"]} \nEmployer: {job["employer"]} \nLocation: {job["location"]} \nPosition Type: {job["position type"]} \nCore Competency: {job["core competency"]}");
+                Console.WriteLine("*****");
+            }
         }
     }
 }
